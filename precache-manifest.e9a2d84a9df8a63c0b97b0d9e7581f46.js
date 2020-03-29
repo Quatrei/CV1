855d69ce04db15dadc803c4128f524f6c4d55ccc
Brian Pineda <BrianPi@magenic.com>
2020-03-29 14:52:38 +0800

    Updates

---
+++ precache-manifest.e9a2d84a9df8a63c0b97b0d9e7581f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac4e76b695e91956ac03b0e642dc670",
+    "url": "/PokedexPlus/index.html"
+  },
+  {
+    "revision": "c292b06cfc067d456adf",
+    "url": "/PokedexPlus/static/css/main.da2e94cc.chunk.css"
+  },
+  {
+    "revision": "b7aa2d6722bbb62caefb",
+    "url": "/PokedexPlus/static/js/2.c290d455.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/PokedexPlus/static/js/2.c290d455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c292b06cfc067d456adf",
+    "url": "/PokedexPlus/static/js/main.68e53d3b.chunk.js"
+  },
+  {
+    "revision": "ee2b02c88d2d12a9a1fe",
+    "url": "/PokedexPlus/static/js/runtime-main.5e066be3.js"
+  }
+]);
